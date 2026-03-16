b00c41d9cae35c8b812de056b337307e72ab77b2
tatianachavesb-ui <tatianachavesb@gmail.com>
2026-03-15 22:10:05 -0500

    Update Finanzas.java

--- Finanzas.java
+++ Finanzas.java
@@ -1,1 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+class Gasto {
+  String descripcion;
+  double valor;
+  String categoria;
+
+  public Gasto(String descripcion, double valor, String categoria){
+    this.descripcion=descripcion;
+    this.valor=valor;
+    this.categoria=categoria;
+  }
+}
+class Ingreso{
+  String descripcion;
+  double valor;
+  String categoria;
+
+  public Ingreso(String descripcion, double valor, String categoria){
+    this.descripcion=descripcion;
+    this.valor=valor;
+    this.categoria=categoria;
+  }
+}
+public class Finanzas{
+  ArrayList<Ingreso> ingresos=new ArrayList<>();
+  ArrayList<Gasto> gastos=new ArrayList<>();
+
+  
+}
