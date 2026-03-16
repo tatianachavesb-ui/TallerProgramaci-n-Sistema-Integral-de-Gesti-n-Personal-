5c3446dd54a218883adca5e758078f0de9b44963
tatianachavesb-ui <tatianachavesb@gmail.com>
2026-03-15 22:03:23 -0500

    Refactor variable declarations for consistency
    Removed unnecessary spaces in variable declarations.

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Finanzas finanzas = new Finanzas();
-        Tareas tareas = new Tareas();
-        Metas metas = new Metas();
+        Scanner sc=new Scanner(System.in);
+        Finanzas finanzas=new Finanzas();
+        Tareas tareas=new Tareas();
+        Metas metas=new Metas();
 
       int opcion;
       System.out.println( "  GESTION PERSONAL ");
