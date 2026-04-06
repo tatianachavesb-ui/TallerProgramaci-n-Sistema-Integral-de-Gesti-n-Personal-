84c44af059543c8b4cbd8926a6b1845547399d28
tatianachavesb-ui <tatianachavesb@gmail.com>
2026-04-06 07:39:45 -0500

    Add methods to register expenses and generate report

--- Finanzas.java
+++ Finanzas.java
@@ -40,5 +40,48 @@ public class Finanzas{
     System.out.println("Ingreso guardado");
   } 
   public void registrarGastos(Scanner sc){
+    System.out.println("\n--- NUEVO GASTO ---");
+        System.out.print("Descripción: ");
+        String desc = sc.nextLine();
+        System.out.print("Valor: $");
+        double val = Double.parseDouble(sc.nextLine());
+        System.out.print("Categoría: ");
+        String cat = sc.nextLine();
+        
+        Gasto gas = new Gasto(desc, val, cat);
+        gastos.add(gas);
+        System.out.println("¡Gasto guardado!");
   }
+  public double calcularBalance() {
+        double totalIngresos = 0;
+        double totalGastos = 0;
+        
+        // Sumar todos los ingresos
+        for (Ingreso i : ingresos) {
+            totalIngresos = totalIngresos + i.valor;
+        }
+        
+        // Sumar todos los gastos
+        for (Gasto g : gastos) {
+            totalGastos = totalGastos + g.valor;
+        }
+        
+        return totalIngresos - totalGastos;
+    }
+
+    // Reporte sencillo
+    public void generarReporte() {
+        System.out.println("\n=== REPORTE FINANCIERO ===");
+        System.out.println("Ingresos:");
+        for (Ingreso i : ingresos) {
+            System.out.println("  + $" + i.valor + " - " + i.descripcion + " (" + i.categoria + ")");
+        }
+        
+        System.out.println("Gastos:");
+        for (Gasto g : gastos) {
+            System.out.println("  - $" + g.valor + " - " + g.descripcion + " (" + g.categoria + ")");
+        }
+        
+        System.out.println("Balance total: $" + calcularBalance());
+    }
 }
